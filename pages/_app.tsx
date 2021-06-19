cd69945d299e4f4b853d73f03e739f5cb60be052
Azorimor <azorimor@gmail.com>
2021-06-19 18:31:18 +0200

    Added PrimeReact with orange theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,8 @@
 import '../styles/globals.scss'
+import 'primeflex/primeflex.css';
+import 'primereact/resources/themes/vela-orange/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
 import { AppProps } from 'next/app'
 
